155ba4724ec8af5b4229b241d67d87a197a821be
Ishaan Bajpai <ishaanbajpai@Ishaans-MacBook-Pro.local>
2021-11-22 12:32:34 -0500

    i2

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 print("Test1 Successful")
